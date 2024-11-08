828706c9d1aa655f5b2fe673c6dea87f5bfdbae6
jerry <jerry0906@naver.com>
2024-11-09 04:33:38 +0900

    Add PlayerController

---
+++ Source/A1/Controller/A1DefaultPlayerController.cpp
@@ -0,0 +1,8 @@
+// Copyright ACCENT, Inc. All Rights Reserved.
+
+
+#include "Controller/A1DefaultPlayerController.h"
+
+
+
+
---
+++ Source/A1/Controller/A1DefaultPlayerController.h
@@ -0,0 +1,20 @@
+// Copyright ACCENT, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "A1DefaultPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class A1_API AA1DefaultPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
